01bafbc960a059a3bdfebf366ff7129068943e9e
Dmaradan <diegomartin1313@gmail.com>
2023-08-16 20:01:42 +0200

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Diego Martin on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
